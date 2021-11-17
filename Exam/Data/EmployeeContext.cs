223142705f0cd41cbc901c7d0af0ec4c248619f9
thu <thu@DESKTOP-KUU4KNM>
2021-11-17 15:54:52 +0700

    Add project files.

---
+++ Exam/Data/EmployeeContext.cs
@@ -0,0 +1,20 @@
+﻿using Exam.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Exam.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+           
+        }
+
+    }
+}
